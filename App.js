a7cf67b399ffdb2f816c0a7448ec26c6ee953afc
Morgan <morganw118@gmail.com>
2021-05-20 16:45:39 -0700

    initialized react native app and installed expo cli packages. Tested changes inn app.js and everything seems to be workinng

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hammy is stinky and smelly. My Chowdie bears is also smelly</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'white'
+  }
+});
