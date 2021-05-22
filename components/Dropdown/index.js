e6219e78ea8c11b6016f6ece311257105e122341
Morgan <morganw118@gmail.com>
2021-05-22 01:18:39 -0700

    made dropdown component and passed tank props for tankmate

---
+++ components/Dropdown/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Picker } from "react-native";
+
+function Dropdown ({ selectedItem, itemOptions, setSelectedValue }) {
+  
+  return (
+    <View>
+      <Picker
+        selectedItem= {selectedItem}
+        style={{ height: 50, width: 150 }}
+        onValueChange={setSelectedValue}
+      >
+        {/* <Picker.Item label="poop" value="java" />
+        <Picker.Item label="JavaScript" value="js" /> */}
+
+        {itemOptions.map((value, index) => {
+          return (
+            <Picker.Item
+              key={value.id}
+              label={value.name} 
+              value={value.id}
+            />
+          )
+        })}
+        
+
+      </Picker>
+    </View>
+  );
+}
+
+export default Dropdown;
--- pages/AddMate1.js
+++ pages/AddMate1.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Dropdown from "../components/Dropdown";
 
 
-const styles = StyleSheet.create({
-    tankPicker: {
-        height: 50,
-        width: 200,
-        backgroundColor: "white",
-        color: "blue"
-    }
-});
+// const styles = StyleSheet.create({
+//     tankPicker: {
+//         height: 50,
+//         width: 200,
+//         backgroundColor: "white",
+//         color: "blue"
+//     }
+// });
 
 class AddMate1 extends Component {
 
@@ -17,17 +18,36 @@ class AddMate1 extends Component {
         super(props)
 
         this.state = {
-            selectedValue: "Tank 1"
+            selectedTank: "hello",
+            tankOptions: [ {id: 1, name: "Tank 1"}, {id: 2, name: "Tank 2"}, {id: 3, name: "Tank 3"} ]
+            // tankOptions: ["Tank 2", "Tank 3", "Tank 4"]
         }
     }
 
+    setSelectedTank = (itemValue, itemIndex) => {
+        this.setState({
+            selectedTank: itemValue
+        }, function() {
+            console.log(this.state.selectedTank)
+        })
+    }
+
+    
     render() {
         return (
             <View >
                 <Text>
                     Add A Tank Mate
                 </Text>
-                <Picker
+                <Dropdown
+                    selectedItem= {this.state.selectedTank}
+
+                    setSelectedValue= {this.setSelectedTank}
+
+                    itemOptions= {this.state.tankOptions}
+
+                />
+                {/* <Picker
                     selectedValue={this.state.selectedValue}
                     style={styles.tankPicker}
                     onValueChange={
@@ -39,7 +59,7 @@ class AddMate1 extends Component {
                 >
                     <Picker.Item label="Tank 1" value="tank1" />
                     <Picker.Item label="Tank 2" value="tank2" />
-                </Picker>
+                </Picker> */}
             </View>
         );
     }
