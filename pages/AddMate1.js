0e5bf061c5a4f2a3dfde555ca0b524db426f06a8
Morgan <morganw118@gmail.com>
2021-05-24 15:13:56 -0700

    added mate type buttons for add mate screen

--- pages/AddMate1.js
+++ pages/AddMate1.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
 import Dropdown from "../components/Dropdown";
 
 
-// const styles = StyleSheet.create({
-//     tankPicker: {
-//         height: 50,
-//         width: 200,
-//         backgroundColor: "white",
-//         color: "blue"
-//     }
-// });
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 5,
+        marginTop: "1rem"
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+        textAlign: "center"
+    },
+   
+});
 
 class AddMate1 extends Component {
 
@@ -19,8 +25,10 @@ class AddMate1 extends Component {
 
         this.state = {
             selectedTank: "hello",
-            tankOptions: [ {id: 1, name: "Tank 1"}, {id: 2, name: "Tank 2"}, {id: 3, name: "Tank 3"} ]
+            tankOptions: [ {id: 1, name: "Tank 1"}, {id: 2, name: "Tank 2"}, {id: 3, name: "Tank 3"} ],
             // tankOptions: ["Tank 2", "Tank 3", "Tank 4"]
+            mateTypes: ["Plant", "Fish", "Amphibian", "Invertebrate"],
+            selectedType: ""
         }
     }
 
@@ -32,35 +40,46 @@ class AddMate1 extends Component {
         })
     }
 
+    // setSelectedType = (value) => {
+    //     this.setState({
+    //         selectedType: value
+    //     }, () => {
+    //         console.log(this.state.selectedType)
+    //     })
+    // }
     
     render() {
         return (
-            <View >
-                <Text>
-                    Add A Tank Mate
-                </Text>
-                <Dropdown
-                    selectedItem= {this.state.selectedTank}
+            <ScrollView>
+                <View >
+                    <Text>Step 1:</Text>
+                    <Text>Choose a tank to add to</Text>
 
-                    setSelectedValue= {this.setSelectedTank}
+                    <Dropdown
+                        selectedItem= {this.state.selectedTank}
+                        setSelectedValue= {this.setSelectedTank}
+                        itemOptions= {this.state.tankOptions}
+                    />
+                </View>
 
-                    itemOptions= {this.state.tankOptions}
+                <View>
+                    <Text>Step 2:</Text>
+                    <Text>Choose the type of mate</Text>
 
-                />
-                {/* <Picker
-                    selectedValue={this.state.selectedValue}
-                    style={styles.tankPicker}
-                    onValueChange={
-                        (itemValue, itemIndex) => 
-                        this.setState({
-                            selectedValue: itemValue
-                        })
-                    }
-                >
-                    <Picker.Item label="Tank 1" value="tank1" />
-                    <Picker.Item label="Tank 2" value="tank2" />
-                </Picker> */}
-            </View>
+                    {this.state.mateTypes.map((elem, index) => {
+                        return (
+                            <TouchableOpacity
+                                // onPress={this.setSelectedType}
+                                style={styles.button}>
+                                <Text style={styles.buttonText}>{elem}</Text>
+                            
+                            </TouchableOpacity>
+                        )
+                    })}
+                </View>
+
+
+            </ScrollView>
         );
     }
 }
